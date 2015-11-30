44de3154fa26031126512eb60d6e70c4df6fdd46
Justin Lee <justintt1995@gmail.com>
2015-11-30 00:02:02 -0500

    night 1

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,15 +3,15 @@
 window.onload = function() {
     var blockly = document.getElementById('blocklyDiv');
     console.log('outside');
+    var tarea = document.getElementById('hack');
 
     var logoutput = function () {
-        console.log('DOPPPPPPPPE');
+        alert("DOPPPPPPPPE");
     }
 
-    blockly.addEventListener('onclick', logoutput);
+    tarea.addEventListener('input', logoutput);
 
-
-    sharejs.open('home', 'text', function(error, doc) {
-            doc.attach_textarea(blockly);
-    });
+   // sharejs.open('home', 'text', function(error, doc) {
+   //          doc.attach_textarea(blockly);
+   //  });
 };
