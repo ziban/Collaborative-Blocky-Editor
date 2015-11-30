e37d7180ea099957ea1680b6c74d80fca0ff4392
ziban <csirmah@gmail.com>
2015-11-30 01:43:19 -0500

    Added the skeleton of a simple install script

---
+++ install.sh
@@ -0,0 +1,67 @@
+# A compile script which when run will execute all the install 
+# and setup functions. It also runs the program for the first time
+
+
+#!/bin/bash
+
+#Checks if a program is installed 
+prog_exists() {
+	type "$1" &> /dev/null;
+}
+
+
+#Check the type of operating system to perform the relevant install
+op_num;
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        op_num = 1;
+        printf "Linux-gnu\n" 
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        op_num = 2; 
+        printf "Mac OS\n"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        op_num = 3;
+        printf "Install not covered at the moment\n"
+elif [[ "$OSTYPE" == "msys" ]]; then
+        op_num = 3;
+        printf "Install not covered at the moment\n"
+elif [[ "$OSTYPE" == "win32" ]]; then
+        op_num = 3;
+        printf "Install not covered at the moment\n"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        op_num = 4;
+        printf "freebsd\n"
+    fi
+        
+
+#Check if there is an error with the installations too
+
+if prog_exists brew; then 
+	printf "Homebrew installed on this computer\n"
+else 
+	printf "Homebrew not installed in this computer\n**Installing**\n"
+fi
+
+
+if prog_exists node; then 
+	printf "Node.js installed on this computer\n"
+else 
+	printf "Node.js  not installed on this computer\n**Installing**\node"
+fi
+
+if prog_exists npm; then 
+	printf "npm installed on this computer\n"
+else 
+	printf "npm  not installed on this computer\n**Installing**\node"
+fi
+
+
+#Clone the repository from github 
+
+
+#Enter into the folder and install all the dependencies 
+
+#Execute the program
+
+
+
