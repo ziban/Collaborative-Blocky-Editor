328dc961f380d7e8aa339f376626e7d6b48e71c9
ziban <csirmah@gmail.com>
2015-11-30 19:15:15 -0500

    Textarea works

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -1,8 +1,10 @@
 # A shell script which when run updates git
 
 #!/bin/bash
-
+git pull
 git add -A
 printf "Please enter the commit message:\n "
 read commit_message 
-git commit -m commit_message
+printf $commit_message
+git commit -m $commit_message
+git push -u origin master
