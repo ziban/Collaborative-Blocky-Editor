00f53aa617c4b458ff5be2a6229632fa56342bb2
ziban <csirmah@gmail.com>
2015-12-02 23:37:37 -0500

    Logging to files now

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
+/**
+*@Author Collins Sirmah and Justin Lee 
+*/
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-//TODO: Delete if no users room implemented
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
-
+var fs = require('fs');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//Logger outputs to a file instead of the console. 
+app.use(logger('common', {
+    stream: fs.createWriteStream('./access.log', {flags: 'a'})
+}));
+
+//Use debugging
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//TODO:Delete if not user rooms implemented
-//app.use('/', routes);
-//app.use('/users', users);
 
 //Implementation for the main page
 app.get('/', function(req, res) {
